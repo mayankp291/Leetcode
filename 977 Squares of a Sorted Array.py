aee2b7f230a0b316d1d03ede21ef671a8ac64a95
Mayank Panjiyara <69447074+mayankp291@users.noreply.github.com>
2022-05-11 16:27:00 +0300

    Create 977 Squares of a Sorted Array.py

---
+++ 977 Squares of a Sorted Array.py
@@ -0,0 +1,18 @@
+# 2 pointer solution
+# Using 2 pointers l and r to converge to the center from both sides.
+# Comparing abs values from both left and right and inserting to another array
+# from right to left
+# O(n) space and time
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        res = [0] * len(nums)
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            if abs(nums[l]) > abs(nums[r]):
+                res[r - l] = nums[l] ** 2
+                l += 1
+            else:
+                res[r - l] = nums[r] ** 2
+                r -= 1
+        return res
+
