9d4f2e75b3fcfb0749801ecfd4a94bfe4e4896c8
Mayank Panjiyara <69447074+mayankp291@users.noreply.github.com>
2022-05-24 22:08:12 +0800

    Create 344 Reverse String.py

---
+++ 344 Reverse String.py
@@ -0,0 +1,11 @@
+# 2 pointers inplace reverse
+# Space O(n) Time O(n)
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        n = len(s)
+        l = 0
+        r = n-1
+        while l<r:
+            s[l], s[r] = s[r], s[l]
+            l+=1
+            r-=1
