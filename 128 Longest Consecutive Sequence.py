c5ea49c2d902b3ce1534fe0acb50b808554e6558
Mayank Panjiyara <69447074+mayankp291@users.noreply.github.com>
2022-05-29 16:06:28 +0800

    Create 128 Longest Consecutive Sequence.py

---
+++ 128 Longest Consecutive Sequence.py
@@ -0,0 +1,19 @@
+# Time O(n), Space O(n)
+# use hashing to and check the longest subsequence
+# Ex: nums = [100,4,200,1,3,2]
+# can be divided into 100, 200, 1->2->3->4
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        n = set(nums)
+        ans = 0
+        length = 0
+        for num in nums:
+            # check if predecessor of a number is in set, 
+            # then it cannot be start of a subsequence
+            if (num - 1) not in n:
+                length = 0
+                # check if it has consecutive numbers
+                while (num + length) in n:
+                    length += 1
+                ans = max(length, ans)
+        return ans
